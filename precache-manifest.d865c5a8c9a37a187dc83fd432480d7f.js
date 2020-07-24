d6a8a23e2c0fa269295d9b358c827e41cead3827
nitinsfdc <nvch66@gmail.com>
2020-07-25 02:14:07 +0530

    Updates

---
+++ precache-manifest.d865c5a8c9a37a187dc83fd432480d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38f33bc93658f19fd4e2e43d14e2116",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "e35a54ee4af46147a038",
+    "url": "/react-game/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "5fc4884d189f47a2dd67",
+    "url": "/react-game/static/js/2.65090e3e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-game/static/js/2.65090e3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35a54ee4af46147a038",
+    "url": "/react-game/static/js/main.a6484870.chunk.js"
+  },
+  {
+    "revision": "aa1f3de6487f8ff7b2dd",
+    "url": "/react-game/static/js/runtime-main.cbcd79c5.js"
+  }
+]);
